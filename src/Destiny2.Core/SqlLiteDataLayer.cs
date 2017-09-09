4d5fc0f98ef27fad5defcbac349114263c4a8828
sharpiro <dsharpbb09@gmail.com>
2017-09-09 12:40:17 -0400

    starting to seperate out code

---
+++ src/Destiny2.Core/SqlLiteDataLayer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+
+namespace Destiny2.Core
+{
+    public class SqlLiteDataLayer
+    {
+        private readonly string _connectionString;
+
+        public SqlLiteDataLayer(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public IEnumerable<object> ExecuteQuery(string query)
+        {
+            using (var conn = new SqliteConnection(_connectionString))
+            using (var command = conn.CreateCommand())
+            {
+                conn.Open();
+                command.CommandText = query;
+                //command.Parameters.Add(new SqliteParameter("", ""));
+                var reader = command.ExecuteReader();
+                //var itemDictionary = new Dictionary<long, JObject>();
+                while (reader.Read())
+                {
+                    for (var i = 0; i < reader.FieldCount; i++)
+                    {
+
+                    }
+                    var temp1 = reader[0];
+                    var temp2 = reader[1];
+                    //var json = JObject.Parse(temp2 as string);
+                    //var hash = (long)json["hash"];
+                    //itemDictionary.Add(hash, json);
+                }
+                conn.Close();
+            }
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
